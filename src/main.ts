7568be1cf038c0bcbb5257634dc54ee5502126ee
Vlad Faust <hey@vladfaust.com>
2022-12-22 02:36:30 +0400

    fix: buffer dependency

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import "./style.scss";
 import App from "./App.vue";
 import router from "./router";
+import { Buffer } from "buffer";
+
+window.Buffer = Buffer;
 
 createApp(App).use(router).mount("#app");
